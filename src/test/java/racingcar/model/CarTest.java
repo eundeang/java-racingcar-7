5f83f5c1f60168e0b99122119836b4d26b803374
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-28 17:09:23 +0900

    test(CarTest): 자동차 이름 입력시 특수문자(구분자(쉼)가 아닌) 또는 공백이 포함되는 경우에 대한 테스트 코드 작성

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -11,6 +11,16 @@ class CarTest {
     private final Car testCar = new Car("testCar");
 
     @Test
+    @DisplayName("경주 자동차 이름 입력 중 특수문자(구분자(쉼)가 아닌) 또는 공백이 포함되는 경우 IllegalArgumentException")
+    void 자동차_이름_입력에_특수문자가_포함된_경우() {
+        Assertions.assertThatThrownBy(() ->
+                        car.splitByDelimiter("happy,for;")
+                )
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
     @DisplayName("자동차 이름이 5글자 이하인 경우 이름 리스트 반환")
     void 이름_수_제한_통과_테스트() {
         Assertions.assertThat(car.splitByDelimiter("four,fivee"))
@@ -29,7 +39,7 @@ class CarTest {
 
     @Test
     @DisplayName("자동차는 전진 상태일때 자신의 이동 현황에 -를 추가한다.")
-    void 전진_상태_일때_저장값_검증_테스트(){
+    void 전진_상태_일때_저장값_검증_테스트() {
         testCar.moveToOrientedDirection(Direction.FORWARD);
         Assertions.assertThat(testCar.getMoveStatus())
                 .isEqualTo("-");
